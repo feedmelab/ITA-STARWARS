9535eb053fec7b3d2ac01dc0f3a0a461a240fa70
feedmelab <xtorner@gmail.com>
2022-03-21 17:23:08 +0100

    news

--- src/pages/Cinema.js
+++ src/pages/Cinema.js
@@ -1,19 +1,39 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { Container } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import Header from "../components/Header";
-import HomeData from "../components/HomeData/HomeData";
 import Nav from "../components/Nav/Nav";
+import ReactPlayer from 'react-player/youtube'
+
 
 export default () => 
-(
-<div className="App">
-    <Header/>
-    <Container className="d-flex justify-content-center align-items-center">
-        <Nav></Nav>
-    </Container>
-    <section>
-        <h3>Choose your way</h3>
-        <div></div>
-    </section>
-</div>
-);
+{
+   const [url, setUrl] = useState("https://www.youtube.com/watch?v=cen-yPSEOiE")
+   const handleURL = (url) => {
+        setUrl(url);
+   }
+    return (
+        <div className="App">
+        <Header/>
+        <Container className="d-flex justify-content-center align-items-center">
+            <Nav></Nav>
+        </Container>
+        <section>
+            <Container className="mt-5 d-flex align-items-center justify-content-center flex-column">
+                <Row className="d-flex flex-column">
+                    <Col>
+                        <ReactPlayer url={url} playing={true} />
+                    </Col>
+                </Row>
+                <Row className="d-flex mt-5">
+                    <Col>
+                        <button className="btn border-0 bg-dark p-1 text-white font-size-base" 
+                        onClick={()=>handleURL("https://www.youtube.com/watch?v=E4iJOpla4AU")}>Jeri Fallen Order</button>
+                        <button className="ms-3 btn border-0 bg-dark p-1 text-white font-size-base" 
+                        onClick={()=>handleURL("https://www.youtube.com/watch?v=cen-yPSEOiE")}>Episode X</button>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+        </div>);
+}
